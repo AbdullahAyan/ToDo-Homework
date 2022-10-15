4b1669d4983a0f395c9bd27e5557b35bba762450
AbdullahAyan <75751013+AbdullahAyan@users.noreply.github.com>
2022-10-15 16:50:27 +0300

    Initial Commit

---
+++ ToDo-Homework/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  ToDo-Homework
+//
+//  Created by Abdullah Ayan on 15.10.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
